7e5dacf45d7f5ecb14713433a455d628e2ed4151
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2023-08-29 18:31:56 +0500

    Added 1 & 2 practical task

--- JavaScript/GB/Lesson_2/script.js
+++ JavaScript/GB/Lesson_2/script.js
@@ -1,1 +1,16 @@
-console.log(String(true) + Number(true));
+// Task 1
+// let num1 = Number(prompt(`Enter first number`));
+// let num2 = Number(prompt(`Enter second number`));
+// if (num1 === 1) {
+//     alert(`${num1} = 1`);
+// } else if (num1 < 1) {
+//     alert(`${num1} < 1`);
+// } else alert(`${num1} > 1`);
+// if (num2 === 3) {
+//     alert(`${num2} = 3`)
+// } else if (num2 > 3) {
+//     alert(`${num2} > 3`)
+// } else alert(`${num2} < 3`)
+// Task 2 
+// let test = true;
+// (test === true) ? console.log('+++'): console.log('---');
