21f0eedb61cbde1ef087b1b8185aa6e31a044ffc
abdulLah <Y@myrizk.ru>
2023-09-15 07:47:22 +0500

    Something added

--- JavaScript/GB/Lesson_5/script.js
+++ JavaScript/GB/Lesson_5/script.js
@@ -1,10 +1,14 @@
-const learnObject = {
-    firstElement: [1, 2, [1, 2, [1, 2, 3]]],
-    secondElement: 2,
-    thirdElemen: 3,
-    fourthElement: 4,
-    fifthElement: 5
-}
-learnObject.sixth = '6';
-delete learnObject.sixth;
-console.log(learnObject);
+// const learnObject = {
+//     firstElement: [1, 2, [1, 2, [1, 2, 3]]],
+//     secondElement: 2,
+//     thirdElemen: 3,
+//     fourthElement: 4,
+//     fifthElement: 5
+// }
+// learnObject.sixth = '6';
+// delete learnObject.sixth;
+// console.log(learnObject);
+
+// const arr = [1, 2, 3, 4, 5];
+// alert(arr.map(i => i));
+// console.log(arr);
