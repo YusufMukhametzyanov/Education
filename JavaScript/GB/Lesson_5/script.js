16c23d45876236b8723df323c165f2e61c8a52d6
unknown <YusufMukhametzyanov@yandex.ru>
2023-09-16 07:32:46 +0500

    Добавили 3 первых примера из теории 5 урока

--- JavaScript/GB/Lesson_5/script.js
+++ JavaScript/GB/Lesson_5/script.js
@@ -1,14 +1,56 @@
-// const learnObject = {
-//     firstElement: [1, 2, [1, 2, [1, 2, 3]]],
-//     secondElement: 2,
-//     thirdElemen: 3,
-//     fourthElement: 4,
-//     fifthElement: 5
-// }
-// learnObject.sixth = '6';
-// delete learnObject.sixth;
-// console.log(learnObject);
+// Если присвоить значение одного объкта другому, то они оба будут ссылаться на один и тот же объект. Example:
 
-// const arr = [1, 2, 3, 4, 5];
-// alert(arr.map(i => i));
-// console.log(arr);
+// const car = {
+//     make: "Audi",
+//     model: "A5",
+//     year: 2023,
+//     color: "red",
+//     passengers: 2,
+//     power: 249,
+// };
+// const myCar = car;
+// console.log(car, myCar);
+// myCar.power = 100;
+// console.log(car)
+
+// В объекты можно добавлять функции, именуются они уже методами. Example:
+
+// const car = {
+//     make: "Audi",
+//     model: "A5",
+//     year: 2023,
+//     color: "red",
+//     passengers: 2,
+//     power: 249,
+//     odometer: 0,
+//     startEngine: function () {
+//         console.log('Engine started');
+//     }
+// };
+// car.startEngine();
+
+// Object metods
+
+// const car = {
+//     make: "Audi",
+//     model: "A5",
+//     year: 2023,
+//     color: "red",
+//     passengers: 2,
+//     power: 249,
+//     odometer: 0,
+//     engineIsStarted: false,
+//     startEngine: function () {
+//         this.engineIsStarted = true;
+//     },
+//     stopEngine: function () {
+//         this.engineIsStarted = false;
+//     },
+//     haveRoadTrip: function (distance) {
+//         if (this.engineIsStarted) {
+//             this.odometer += distance;
+//         } else {
+//             alert(`Сначала запустите двигатель! // startEngine();`)
+//         }
+//     },
+// };
