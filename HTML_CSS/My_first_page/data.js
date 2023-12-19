974781e9df2e367cb1a6ea1f42ab0fe1ff754ca8
unknown <Y@myrizk.ru>
2023-12-20 00:26:15 +0500

    Added result of the task for 11 lesson

---
+++ HTML_CSS/My_first_page/data.js
@@ -0,0 +1,44 @@
+const featuredItems = `[
+  {
+    "title": "ELLERY X M'O CAPSULE",
+    "img": "img/body4_picture1.png",
+    "discription":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+  },
+  {
+    "title": "ELLERY X M'O CAPSULE",
+    "img": "img/body4_picture2.png",
+    "discription":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+  },
+  {
+    "title": "ELLERY X M'O CAPSULE",
+    "img": "img/body4_picture3.png",
+    "discription":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+  },
+  {
+    "title": "ELLERY X M'O CAPSULE",
+    "img": "img/body4_picture4.png",
+    "discription":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+  },
+  {
+    "title": "ELLERY X M'O CAPSULE",
+    "img": "img/body4_picture5.png",
+    "discription":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+  },
+  {
+    "title": "ELLERY X M'O CAPSULE",
+    "img": "img/body4_picture6.png",
+    "discription":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+  }
+]`;
--- HTML_CSS/My_first_page/script.js
+++ HTML_CSS/My_first_page/script.js
@@ -1,5 +1,57 @@
-const theBrand = document.querySelector('.the_brand');
-theBrand.setAttribute('title', 'The brand');
-theBrand.onclick = function () {
-    theBrand.textContent = 'THE brand';
-}
+const featuredItemsJS = JSON.parse(featuredItems);
+console.log(featuredItemsJS);
+
+const contentItems = document.querySelectorAll(".content1_image1");
+const contentItem2 = document.querySelector(".content2_top_background");
+const contentTopBackground = document.querySelector(".content2_top_background");
+
+let count = 0;
+
+featuredItemsJS.forEach((element) => {
+  contentItems[count].insertAdjacentHTML(
+    "beforeend",
+    `<img src="${element.img}" alt="picture_man" class="body4_picture1">
+    <a href="#" class="content1_active">
+            <img src="img/content1_active_img.png" alt="" class="content1_active_img">
+            <p class="content1_active_p">Add to Cart</p>
+    </a>
+`
+  );
+
+  contentItems[count].nextElementSibling.insertAdjacentHTML(
+    "beforeend",
+    `
+  <div class="content1__bottom">
+    <p class="content1_text1"><strong>${element.title}</strong><br><br></p>
+    <p class="content1_text2">${element.discription}</p><br>
+    <p class="content1_text3">${element.price}</p>
+  </div>
+  `
+  );
+
+  count++;
+
+  if (element.img === "img/body4_picture2.png") {
+    contentItem2.insertAdjacentHTML(
+      "beforeend",
+      `<img src="${element.img}" alt="picture_women" class="content2_top_backgroun_img">
+      <a href="#" class="content1_active">
+            <img src="img/content1_active_img.png" alt="" class="content1_active_img">
+            <p class="content1_active_p">Add to Cart</p>
+      </a>
+    `
+    );
+
+    contentItem2.nextElementSibling.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="content1__bottom">
+      <p class="content1_text1"><strong>${element.title}</strong><br><br></p>
+      <p class="content1_text2">${element.discription}</p><br>
+      <p class="content1_text3">${element.price}</p>
+    </div>
+    `
+    );
+    count = 1;
+  }
+});
